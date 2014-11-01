8c5025d57db04c9de9f7a4186aff4cfc2f979cfa
Jesse Buchanan <hello@jessebuchanan.ca>
2014-11-01 01:00:31 -0400

    Refactor adapter to not be coupled to reddit's JSON response

--- app/adapters/subreddit.js
+++ app/adapters/subreddit.js
@@ -5,6 +5,16 @@ export default Ember.Object.extend({
   find: function(name, id) {
     // subreddit
     // aww
-    return ajax("https://www.reddit.com/r/" + id + ".json");
+    var url = "https://www.reddit.com/r/" + id + ".json";
+    return ajax(url).then(function(result) {
+      return result.data.children.map(function(c) {
+        return {
+           id: c.id,
+           title: c.data.title,
+           domain: c.data.domain,
+           url: c.data.url
+        };
+      });
+    });
   }
 });
